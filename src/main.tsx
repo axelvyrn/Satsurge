2863193c023744f198bb973179a0b8ff737d114e
Axel Vyrn <ghoshgairik2020@gmail.com>
2025-09-22 16:07:09 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { Buffer } from 'buffer'
+import { Buffer } from 'buffer';
 
 // Make Buffer global
 if (!window.Buffer) {
-  window.Buffer = Buffer
+  window.Buffer = Buffer;
 }
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
