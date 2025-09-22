5c17d5215037ecc366d3e8b7b0af4469a9a46c53
Axel Vyrn <ghoshgairik2020@gmail.com>
2025-09-22 16:03:17 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Buffer } from 'buffer';
-window.Buffer = Buffer;
-
 import App from './App.tsx';
 import './index.css';
+window.Buffer = Buffer;
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
